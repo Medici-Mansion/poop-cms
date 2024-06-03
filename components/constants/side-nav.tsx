1038767128e308da43a5decf1e9beb29f07a0ab3
JeongbinYoon <yon980824@gmail.com>
2024-06-04 00:27:13 +0900

    feat: 기본 UI 설정

--- components/constants/side-nav.tsx
+++ components/constants/side-nav.tsx
@@ -1,38 +1,55 @@
-import { BookOpenCheck, LayoutDashboard } from "lucide-react";
-import { type NavItem } from "types";
+import {
+  BookOpenCheck,
+  LayoutDashboard,
+  LucideUsersRound,
+  LibraryBig,
+} from 'lucide-react';
+import { type NavItem } from 'types';
 
 export const NavItems: NavItem[] = [
   {
-    title: "Dashboard",
+    title: '대시보드',
     icon: LayoutDashboard,
-    href: "/",
-    color: "text-sky-500",
+    href: '/',
+    color: 'text-sky-500',
   },
   {
-    title: "Example",
-    icon: BookOpenCheck,
-    href: "/example",
-    color: "text-orange-500",
-    isChidren: true,
-    children: [
-      {
-        title: "Example-01",
-        icon: BookOpenCheck,
-        color: "text-red-500",
-        href: "/example/1",
-      },
-      {
-        title: "Example-02",
-        icon: BookOpenCheck,
-        color: "text-red-500",
-        href: "/example/2",
-      },
-      {
-        title: "Example-03",
-        icon: BookOpenCheck,
-        color: "text-red-500",
-        href: "/example/3",
-      },
-    ],
+    title: '회원 관리',
+    icon: LucideUsersRound,
+    href: '/',
+    color: 'text-orange-500',
   },
+  {
+    title: '리소스 관리',
+    icon: LibraryBig,
+    href: '/',
+    color: 'brown-orange-500',
+  },
+  // {
+  //   title: 'Example',
+  //   icon: BookOpenCheck,
+  //   href: '/example',
+  //   color: 'text-orange-500',
+  //   isChidren: true,
+  //   children: [
+  //     {
+  //       title: 'Example-01',
+  //       icon: BookOpenCheck,
+  //       color: 'text-red-500',
+  //       href: '/example/1',
+  //     },
+  //     {
+  //       title: 'Example-02',
+  //       icon: BookOpenCheck,
+  //       color: 'text-red-500',
+  //       href: '/example/2',
+  //     },
+  //     {
+  //       title: 'Example-03',
+  //       icon: BookOpenCheck,
+  //       color: 'text-red-500',
+  //       href: '/example/3',
+  //     },
+  //   ],
+  // },
 ];
--- components/dashboard/recent-sales.tsx
+++ components/dashboard/recent-sales.tsx
@@ -10,12 +10,10 @@ export function RecentSales() {
           <AvatarFallback>OM</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
-          <p className="text-sm text-muted-foreground">
-            olivia.martin@email.com
-          </p>
+          <p className="text-sm font-medium leading-none">신고해요</p>
+          <p className="text-sm text-muted-foreground">2024.06.01</p>
         </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
+        <div className="ml-auto font-medium">욕설</div>
       </div>
       <div className="flex items-center">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
@@ -23,10 +21,10 @@ export function RecentSales() {
           <AvatarFallback>JL</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+          <p className="text-sm font-medium leading-none">신고해요</p>
+          <p className="text-sm text-muted-foreground">2024.06.01</p>
         </div>
-        <div className="ml-auto font-medium">+$39.00</div>
+        <div className="ml-auto font-medium">욕설</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
@@ -34,12 +32,10 @@ export function RecentSales() {
           <AvatarFallback>IN</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-          <p className="text-sm text-muted-foreground">
-            isabella.nguyen@email.com
-          </p>
+          <p className="text-sm font-medium leading-none">신고해요</p>
+          <p className="text-sm text-muted-foreground">2024.06.01</p>
         </div>
-        <div className="ml-auto font-medium">+$299.00</div>
+        <div className="ml-auto font-medium">욕설</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
@@ -47,10 +43,10 @@ export function RecentSales() {
           <AvatarFallback>WK</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">William Kim</p>
-          <p className="text-sm text-muted-foreground">will@email.com</p>
+          <p className="text-sm font-medium leading-none">신고해요</p>
+          <p className="text-sm text-muted-foreground">2024.06.01</p>
         </div>
-        <div className="ml-auto font-medium">+$99.00</div>
+        <div className="ml-auto font-medium">욕설</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
@@ -58,10 +54,10 @@ export function RecentSales() {
           <AvatarFallback>SD</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+          <p className="text-sm font-medium leading-none">신고해요</p>
+          <p className="text-sm text-muted-foreground">2024.06.01</p>
         </div>
-        <div className="ml-auto font-medium">+$39.00</div>
+        <div className="ml-auto font-medium">욕설</div>
       </div>
     </div>
   );

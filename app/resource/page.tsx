0b9e8e3b98e060e9b4fc1179568d5ae45c87df21
JeongbinYoon <yon980824@gmail.com>
2024-07-26 20:20:14 +0900

    feat: Tab 컴포넌트 tabType 추가

--- app/resource/page.tsx
+++ app/resource/page.tsx
@@ -6,11 +6,15 @@ import React from 'react';
 const DogsPage = () => {
   return (
     <div className="flex h-full flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <Tabs defaultValue="dogs" className="space-y-4">
-          <TabsList>
-            <TabsTrigger value="dogs">Dogs</TabsTrigger>
-            <TabsTrigger value="graphics">Graphics</TabsTrigger>
+      <div className="flex-1 space-y-4 p-8 pt-24">
+        <Tabs defaultValue="dogs" className="space-y-16">
+          <TabsList tabType="menu">
+            <TabsTrigger tabType="menu" value="dogs">
+              Dogs
+            </TabsTrigger>
+            <TabsTrigger tabType="menu" value="graphics">
+              Graphics
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="dogs" className="space-y-4">
             <Breeds />
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,41 +1,45 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
 
-import { cn } from "lib/utils"
+import { cn } from 'lib/utils';
 
-const Tabs = TabsPrimitive.Root
+const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List> & {
+    tabType: string;
+  }
+>(({ className, tabType, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
-      className
+      `inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-custom-gray-300 ${tabType === 'menu' && 'bg-none!'}`,
+      className,
     )}
     {...props}
   />
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> & {
+    tabType: string;
+  }
+>(({ className, tabType, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
-      className
+      `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm ${tabType === 'menu' && 'text-5xl px-0 mr-8'}`,
+      className,
     )}
     {...props}
   />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
@@ -44,12 +48,12 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className
+      'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      className,
     )}
     {...props}
   />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
